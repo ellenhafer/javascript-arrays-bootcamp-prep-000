9acef60b056a262a573d04b4302ecd0255905086
Ellen O'Neill <ellen.o.neill.munro@gmail.com>
2018-05-23 14:04:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,11 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  var newArray = ['element', ...array]
+  var newArray = [element, ...array]
+  return newArray
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift('element');  
+  array.unshift(element);  
+  return array
 }
